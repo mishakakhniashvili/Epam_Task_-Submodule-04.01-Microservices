2bfef44937123b0aef5f5ef461b0a9b2f337be76
Mikheili Kakhniashvili <mikheili_kakhniashvili@epam.com>
2026-07-19 02:56:49 +0400

    Add trainer workload service skeleton

---
+++ trainer-workload-service/src/test/java/com.epam.trainerworkload/TrainerWorkloadApplicationTest.java
@@ -0,0 +1,11 @@
+package com.epam.trainerworkload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TrainerWorkloadApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
